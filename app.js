15078a23b8e88b2fa1a12530deaab81e08654c34
mhl <1340782811@qq.com>
2018-12-17 10:36:28 +0800

    修改了文件

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 //导包
 const express = require("express");
+const router = require("./router");
+const bodyPqrser = require("body-parser");
+
+//配置
+//实例化app
+const app = express();
+//统一处理自己的静态资源
+app.use("/public" ,express.static("./public"));
+
+//处理第三方静态资源
+app.use("/node-module", express.static("./node_modules"));
+
+//配置模板引擎
+app.engine('html', require('express-art-template'));
+
+app.use(bodyPqrser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyPqrser.json());
+
+//挂载路由
+app.use(router);
+
+//监听端口
+app.listen(8000, () => {
+    console.log('please run it ......');
+})
